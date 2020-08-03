e0a6ec719b3ebe1e6cd7854266501a47139d4671
Will <wttgash489@gmail.com>
2020-08-03 09:21:43 +0100

    commiting pig game changes on pig game branch

--- CompleteJSCourse2020/JSInTheBrowser/Dom-Pig-Game/app.js
+++ CompleteJSCourse2020/JSInTheBrowser/Dom-Pig-Game/app.js
@@ -9,29 +9,132 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
 
-dice = Math.floor(Math.random()*6) + 1;
-console.log(dice);
+init();
+
+
+
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
 
 // this allows us to select elements in exactly the same way as we do
 // in css. The only difference being it only selects the first element that it finds
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
-
 var x = document.querySelector('#score-0').textContent;
 console.log(x);
 
-document.querySelector('.dice').style.display = 'none';
 
 
 
+// function btn() {
+//     //Do something
+// }
 
+// in this example btn is a 'call-back function' because it is not
+// called by us, but by another function. 
+// it is a function that we pass into another function as an argument
+// document.querySelector('.btn-roll').addEventListener('click', btn);
+
+/* alternatively we can achieve the same thing without using an external function.
+  in this case we would write the function within the brackets as an argument
+ this is what we call an 'anonamous function' - because it doesn't have a name 
+ and cannot be reused.
+*/
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    // 1. random number
+    var dice = Math.floor(Math.random()*6) + 1;
+    // 2. Display result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    // 3. Update the round score IF the rolled number was not a 1
+    if (dice !== 1) {
+        //add score
+        roundScore += dice;
+        // display updated value in UI
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+
+    } else {
+        // next player
+        nextPlayer();
+
+    }
+});
+
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    //add current score to players global score
+    scores[activePlayer] += roundScore;
+    // update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    // check if player won the game
+    if (scores[activePlayer] >= 100) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    } else {
+        //change active player
+        nextPlayer()
+    }
+   
+
+})
+
+//implement a whole new function for chaning player to avoid repeating code - DRY principle
+function nextPlayer() {
+    // use the tertiary operator
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    /* same as writing 
+         if(activePlayer === 0) {
+             activePlayer = 1;
+         } else {
+             activePlayer = 0;
+         }
+     */
+     roundScore = 0;
+     //update UI
+     document.getElementById('current-0').textContent = '0';
+     document.getElementById('current-1').textContent = '0';
+     
+     // update UI to show active player
+     // classList.toggle is like using an if statement to check which player
+     // is currently active. 
+     document.querySelector('.player-0-panel').classList.toggle('active');
+     document.querySelector('.player-1-panel').classList.toggle('active');
+
+     // update UI to hide dice
+     document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+// init function for new window / new game
+function init() {
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    // hide the dice
+    document.querySelector('.dice').style.display = 'none';
+
+    // initialise all values in the game to read 0
+    // getElementById is faster than querySelector
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('#name-0').textContent = 'Player 1';
+    document.querySelector('#name-1').textContent = 'Player 2';
+    
+
+
+}
 
 
 
